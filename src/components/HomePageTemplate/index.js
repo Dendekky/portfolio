68947ac6078c1bc656f2b04925ed94e4a4b0bb0c
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2020-06-26 12:34:33 +0100

    initial commit

--- src/components/HomePageTemplate/index.js
+++ src/components/HomePageTemplate/index.js
@@ -18,13 +18,13 @@ const HomePageTemplate = ({
       <title>{meta_title}</title>
       <meta name='description' content={meta_description} />
     </Helmet>
-    <section className='hero is-primary is-bold is-medium'>
+    <section className='hero bg-hero is-bold is-medium'>
       <div className='hero-body'>
         <div className='container'>
           <div className='columns'>
-            <div className='column is-10 is-offset-1'>
+            <div className='column is-12'>
               <div className='section'>
-                <h1 className='title'>
+                <h1 className='title primary-text has-text-centered'>
                   {title}
                 </h1>
               </div>
