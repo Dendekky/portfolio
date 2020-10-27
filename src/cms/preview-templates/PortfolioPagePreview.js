bcffd819c538d4b83b41e879584f91fffd5e1082
Ibrahim <adeniyiib360@gmail.com>
2020-10-27 16:13:32 +0100

    fix: found and removed file causing creation of null pages :bug:

--- src/cms/preview-templates/PortfolioPagePreview.js
+++ src/cms/preview-templates/PortfolioPagePreview.js
@@ -3,23 +3,23 @@ import PropTypes from 'prop-types'
 import PortfolioPageTemplate from '../../components/PortfolioPageTemplate'
 
 const PricingPagePreivew = ({ entry, getAsset }) => {
-  const entryPricingPlans = entry.getIn(['data', 'portfolio', 'plans'])
-  const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
+  // const entryPricingPlans = entry.getIn(['data', 'portfolio', 'plans'])
+  // const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
 
-  const entryProjects = entry.getIn(['data', 'projects'])
-  const projects = entryProjects ? entryProjects.toJS() : []
+  // const entryProjects = entry.getIn(['data', 'projects'])
+  // const projects = entryProjects ? entryProjects.toJS() : []
 
   return (
     <PortfolioPageTemplate
       title={entry.getIn(['data', 'title'])}
       meta_title={entry.getIn(['data', 'meta_title'])}
       meta_description={entry.getIn(['data', 'meta_description'])}
-      portfolio={{
-        heading: entry.getIn(['data', 'portfolio', 'heading']),
-        description: entry.getIn(['data', 'portfolio', 'description']),
-        plans: pricingPlans,
-      }}
-      projects={projects}
+      // portfolio={{
+      //   heading: entry.getIn(['data', 'portfolio', 'heading']),
+      //   description: entry.getIn(['data', 'portfolio', 'description']),
+      //   plans: pricingPlans,
+      // }}
+      // projects={projects}
     />
   )
 }
