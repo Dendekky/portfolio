4705a03ff64f8cd902fa77fdf6ff7a7187278e66
Ibrahim <adeniyiib360@gmail.com>
2020-10-28 23:02:24 +0100

    update config file

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  siteTitle: `Dendekky's personal website `, // Site title.
-  siteTitleAlt: 'Personal', // Alternative site title for SEO.
+  siteTitle: `Dendekky's portfolio website `, // Site title.
+  siteTitleAlt: 'Dendekky', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://dendekky.me', // Domain of your website without pathPrefix.
+  siteUrl: 'https://dendekky.netlify.app', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Portfolio website', // Website description used for RSS feeds/meta description tag.
+  siteDescription: `Dendekky's Portfolio website`, // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
